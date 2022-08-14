fc5417edfb074edd91d2ecd3245eff6f3d0e34ff
Gyurkovics Ferenc <gyurkovicsferi@gmail.com>
2022-08-14 20:27:53 +0200

    refactor: remove comment

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ void main() {
 class GuitarNotesApp extends StatelessWidget {
   const GuitarNotesApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
